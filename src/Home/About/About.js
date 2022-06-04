e5ad5753d96f1988e28db847682b8176a4ddbeb3
Noor-26 <nk26982698@gmail.com>
2022-06-04 21:14:59 +0600

    scroll animaiton added on about section

--- src/Home/About/About.js
+++ src/Home/About/About.js
@@ -2,39 +2,39 @@ import React from 'react'
 import './About.css'
 
 function About() {
-  return (
-    <div  data-aos="fade-up"
-    data-aos-anchor-placement="top-center"
-    data-aos-duration="1000">
+    return (
+        <div data-aos="fade-up"
+            data-aos-anchor-placement="top-center"
+            data-aos-duration="1000">
             <div >
-            <h5 className="mt-3 pt-3 text">About</h5>
-            <h2 id="About" className='pt-1' >Personal Info</h2>
-            <h5 className='pt-2'>Get to know more about me</h5>
+                <h5 className="mt-3 pt-3 text">About</h5>
+                <h2 id="About" className='pt-1' >Personal Info</h2>
+                <h5 className='pt-2'>Get to know more about me</h5>
             </div>
             <div className="row mx-auto row-cols-1 row-cols-md-2 container my-5" >
-        <div className="col">
-          <h3 className="text-start">Hello!</h3>
-          <p className="text-justify">My name is Mahtab and i am a passtionate Frontend Web Developer using web technologies to build amazing projects and focusing on solving problems for different industruies using the power of technology.
-          </p>
-            <p className="text-start">i will love to hear from you .Wheteher its a project, job opportunatity or just a chat, Feel free to contact me.
-            </p>
+                <div className="col">
+                    <h3 className="text-start">Hello!</h3>
+                    <p className="text-justify">My name is Mahtab and i am a passtionate Frontend Web Developer using web technologies to build amazing projects and focusing on solving problems for different industruies using the power of technology.
+                    </p>
+                    <p className="text-start">i will love to hear from you .Wheteher its a project, job opportunatity or just a chat, Feel free to contact me.
+                    </p>
+                </div>
+                <div class="col  ">
+                    <h3 >skils</h3>
+                    <div className="skils container pt-3 ps-5">
+                        <p class="skil">HTML</p>
+                        <p class="skil">CSS</p>
+                        <p class="skil">Javascript</p>
+                        <p class="skil">React</p>
+                        <p class="skil">Bootstrap</p>
+                        <p class="skil">tailwind</p>
+                        <p class="skil">Nodejs</p>
+                        <p class="skil">MongoDB</p>
+                    </div>
+                </div>
+            </div>
         </div>
-        <div class="col  ">
-        <h3 >skils</h3>
-        <div className="skils container pt-3 ps-5">
-        <p class="skil">HTML</p>
-        <p class="skil">CSS</p>
-        <p class="skil">Javascript</p>
-        <p class="skil">React</p>
-        <p class="skil">Bootstrap</p>
-        <p class="skil">tailwind</p>
-        <p class="skil">Nodejs</p>
-        <p class="skil">MongoDB</p>
-        </div>
-        </div>
-      </div>
-        </div>
-  )
+    )
 }
 
 export default About
