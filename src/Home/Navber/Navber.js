73f7b558186b16efe96c246e578152bf6576a331
Noor-26 <nk26982698@gmail.com>
2022-06-04 20:17:35 +0600

    initial setup done and navber created

---
+++ src/Home/Navber/Navber.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import { Navbar,Container,Nav,} from 'react-bootstrap';
+
+function Navber() {
+  return (
+    <div >
+    <Navbar bg="light" variant="light" fixed="top" >
+<Container>
+<Navbar.Brand href="#home" className="n-h1">Darkfam</Navbar.Brand>
+<Nav className="ms-auto">
+<Nav.Link href="#home">Home</Nav.Link>
+<Nav.Link href="#About">About</Nav.Link>
+<Nav.Link href="#Project">Projects</Nav.Link>
+</Nav>
+</Container>
+</Navbar>
+</div>
+  )
+}
+
+export default Navber
