86e65975ad79c04a4bc35e63aa5dd66a88bcb41c
Noor-26 <nk26982698@gmail.com>
2022-06-04 22:25:45 +0600

    animation added on projects cards

--- src/Home/Projects/ProjectCard.js
+++ src/Home/Projects/ProjectCard.js
@@ -4,13 +4,13 @@ function ProjectCard({project}) {
     const {img,name,description,visit,code} = project
   return (
     <div>
-            <div className="row row-cols-1 container mx-auto row-cols-md-2  my-5"  >
+            <div className="row row-cols-1 container mx-auto row-cols-md-2  my-5"   data-aos="fade-up-right"  data-aos-duration="1000">
             <div className="col p-cols">
         <div className="card h-100 ">
     <img src={img} alt="project-image" className="card-img-top project-img p-4"/>
         </div>
       </div>
-      <div className="col p-cols" >
+      <div className="col p-cols"  data-aos="fade-up-left"  data-aos-duration="1000">
         <div className="card h-100 ">
           <div className="card-body pt-5">
             <h5 className="card-title text-center mt-2  ">{name}</h5>
