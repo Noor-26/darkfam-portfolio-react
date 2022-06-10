490781192068426e5e325c951dc6d4d00b254eac
Noor-26 <nk26982698@gmail.com>
2022-06-10 11:18:40 +0600

    resposive nav and resume btn added

--- src/Home/Banner/Banner.js
+++ src/Home/Banner/Banner.js
@@ -5,23 +5,27 @@ function Banner() {
     const projectPage = () => window.location.href = "#Project"
     const aboutPage = () => window.location.href = "#About"
     return (
-        <div className="main "  id='home'>
+        <div className="main " id='home'>
             <img src="images/background.png" alt=" background img" className="img-fluid bg-img" />
             <div className=" ms-4 ms-md-5 main d-flex align-items-center " id="main-content">
                 <div className="row row-cols-1  row-cols-md-2 w-100 ">
-                    <div className="main-txt col" >
+                    <div className="main-txt col " >
                         <h5 className="main-txt-h3 text-start greet ">Hello there!</h5>
-                        <h1 className="text-start m-h1 font"><Typewriter  options={{
-    autoStart:true,
-    loop:true,
-    delay:40,
-    strings:["I am Mahtab","I am Darkfam"]
-}}/></h1>
-                        <h2 className="text-start font">I am a frontend web developer</h2>
-
+                        <h1 className="text-start m-h1 font d-flex gx-3"> I am <span className='ms-2'>
+                            <Typewriter options={{
+                                autoStart: true,
+                                loop: true,
+                                delay: 40,
+                                strings: [" Mahtab", " Darkfam"]
+                            }} />
+                        </span>
+                        </h1>
+                        <h2 className="text-start font">I am a Frontend Web Developer</h2>
+                        <div className='text-start'>
+                            
+                            <a href="https://drive.google.com/file/d/1NcMcLOlHxzGKwotRqoxDYwDx1hJ3THhd/view?usp=sharing"  target="_blank"> <button className="  button my-3" >Download Resume</button></a>
+                        </div>
                     </div>
-
-
                     <div className="col" >
                         <div>
 
@@ -29,7 +33,6 @@ function Banner() {
                         </div>
                         <button className="button" onClick={aboutPage}>More about me</button>
                     </div>
-
                 </div>
             </div>
         </div>
--- src/Home/Navber/Navber.js
+++ src/Home/Navber/Navber.js
@@ -4,16 +4,19 @@ import { Navbar,Container,Nav,} from 'react-bootstrap';
 function Navber() {
   return (
     <div >
-    <Navbar bg="light" variant="light" fixed="top" >
-<Container>
-<Navbar.Brand href="#home" className="n-h1 fs-3 ">Darkfam</Navbar.Brand>
-<Nav className="ms-auto fs-5">
-<Nav.Link href="#home" >Home</Nav.Link>
-<Nav.Link href="#About">About</Nav.Link>
-<Nav.Link href="#Project">Projects</Nav.Link>
-</Nav>
-</Container>
-</Navbar>
+<Navbar collapseOnSelect expand="lg" bg="light" variant="light">
+      <Container>
+      <Navbar.Brand href="#" className="n-h1 fs-3 ">Darkfam</Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="ms-auto"> 
+          <Nav.Link href="#home" >Home</Nav.Link>
+          <Nav.Link href="#About">About</Nav.Link>
+          <Nav.Link href="#Project">Projects</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
 </div>
   )
 }
